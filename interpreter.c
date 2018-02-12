a02437e83692a747a3d84810b7bf8196fff7180b
UTAGAWA Kiki <utagawakiki@gmail.com>
2018-02-12 16:38:18 +0900

    bf interpreter

---
+++ interpreter.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+static char *p, *ptr;
+static char vars[30000];
+
+void eval () {
+  char *oldptr;
+  while (*p != '\0') {
+    switch (*p) {
+      case '+':
+        (*ptr)++;
+        break;
+      case '-':
+        (*ptr)--;
+        break;
+      case '>':
+        ptr++;
+        break;
+      case '<':
+        ptr--;
+        break;
+      case '.':
+        putchar(*ptr);
+        break;
+      case ',':
+        *ptr = getc(stdin);
+        break;
+      case '[':
+        oldptr = p++;
+        eval();
+        // if nonzero
+        if (*ptr)
+          p = oldptr - 1;
+        break;
+      case ']':
+        // end of loop
+        return;
+      default:
+        break;
+    }
+    p++;
+  }
+}
+
+int main (int argc, char **argv) {
+  // initialize variables
+  memset(vars, 0, sizeof vars);
+  if (argc < 2) {
+    printf("Usage: ./bf [program]\n");
+    return 1;
+  }
+  p = argv[1];
+  ptr = vars;
+  eval();
+  return 0;
+}
